1941ddbe37c48665f90f6e264462d1a63409f848
Everesh <everesh@proton.me>
2024-04-19 20:40:43 +0200

    Add sassy playRound

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,3 +6,16 @@ function getComputerChoice() {
 function getPlayerChoice() {
     return prompt("Rock, Paper, Scissors. Choose one. Do it now!").toLowerCase().trim();
 }
+
+function playRound() {
+    let player = getPlayerChoice();
+    if (player != "rock" && player != "paper" && player != "scissors"){
+        return `You heathen! "${player}"?! What sort of response is that?`;
+    }
+    let computer = getComputerChoice();
+    return (player == computer) ? `${player}, eh? Good choice. I made the same... Its a DRAW!` :
+           (player == "rock" && computer == "scissors"
+           || player == "paper" && computer == "rock"
+           || player == "scissors" && computer == "paper") ? `${player.toUpperCase()}?! No way! My poor innocent ${computer}... Its your WIN!` :
+           `${player}, huh? Pathetic. My ${computer} crushes your feeble pick.. Its your LOSE!`;
+}
