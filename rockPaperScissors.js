767676c8e1d6b18f2a8b5b152b8d70065930232f
Everesh <everesh@proton.me>
2024-04-19 20:27:15 +0200

    Add getPlayerChoice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,3 +2,7 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     return (random == 0) ? "rock" : (random == 1 ) ? "paper" : "scissors";
 }
+
+function getPlayerChoice() {
+    return prompt("Rock, Paper, Scissors. Choose one. Do it now!").toLowerCase().trim();
+}
