0c5fe2ac4288d6c43761615ed0df6ab9962579d4
Everesh <everesh@proton.me>
2024-04-24 22:48:39 +0200

    Depreciate playGame()

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -19,34 +19,3 @@ function playRound() {
            || player == "scissors" && computer == "paper") ? `${player.toUpperCase()}?! No way! My poor innocent ${computer}... Its your WIN!` :
            `${player}, huh? Pathetic. My ${computer} crushes your feeble pick.. Its your LOSE!`;
 }
-
-function playGame() {
-    let player = 0;
-    let computer = 0;
-    let parseMe = ""
-    for (let cycle = 0; cycle < 5; cycle++) {
-        parseMe = playRound();
-        if (parseMe.includes("WIN")) {
-            player++;
-        }
-        else if (parseMe.includes("LOSE")) {
-            computer++;
-        }
-        else {
-            player++;
-            computer++;
-        }
-        console.log(parseMe);
-        console.log(`It's ${player}:${computer}`);
-    }
-    if (player == computer) {
-        console.log(`${player}:${computer} nailbitter right? Looks like neither came on top, its an overall DRAW!`)
-    }
-    else if (player > computer) {
-        console.log(`Whoa, good job! ${player}:${computer} that's an overall WIN!`);
-    }
-    
-    else {
-        console.log(`Oh... Such a shame. ${player}:${computer} looks like overall your LOSE!`);
-    }
-}
