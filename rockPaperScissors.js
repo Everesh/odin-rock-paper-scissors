46eef4dced567803fa379c4aa7e1e7c133ac72e4
Everesh <everesh@proton.me>
2024-04-24 23:13:21 +0200

    Depreciate getPlayerChoice()

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -3,12 +3,7 @@ function getComputerChoice() {
     return (random == 0) ? "rock" : (random == 1 ) ? "paper" : "scissors";
 }
 
-function getPlayerChoice() {
-    return prompt("Rock, Paper, Scissors. Choose one. Do it now!").toLowerCase().trim();
-}
-
 function playRound(player) {
-    //let player = getPlayerChoice();
     if (player != "rock" && player != "paper" && player != "scissors"){
         return `You heathen! "${player}"?! What sort of response is that?`;
     }
